c728d87b027e2dee36b56b78fb92544f5502436b
Maki Aboabida <maki-miko@hotmail.com>
2021-08-09 21:04:47 -0400

    added form submition

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form") //form element
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = () => {
+var createTaskHandler = (event) => {
+  event.preventDefault()      // prevents page from refreshing
+
   var listItemEl = document.createElement("li");  //   Create a new task item.
   listItemEl.className = "task-item";   // Style the new task item.
   listItemEl.textContent = "This is a new task."; // Add the text.
   tasksToDoEl.appendChild(listItemEl);  // Append this element to the task list.
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
