3ff5bb35b9f27369a9d6d026f4079d625a9b1cc8
Maki Aboabida <maki-miko@hotmail.com>
2021-08-12 13:47:17 -0400

    Made button get input values and display them in the task list

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,27 @@ var formEl = document.querySelector("#task-form") //form element
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = (event) => {
-  event.preventDefault()      // prevents page from refreshing
+  event.preventDefault(event)
 
-  var listItemEl = document.createElement("li");  //   Create a new task item.
-  listItemEl.className = "task-item";   // Style the new task item.
-  listItemEl.textContent = "This is a new task."; // Add the text.
-  tasksToDoEl.appendChild(listItemEl);  // Append this element to the task list.
+  // getting values 
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value; 
+
+  // list 
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+
+  // div 
+  var taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
+
+  // add HTML content to page  
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+  listItemEl.appendChild(taskInfoEl);
+
+  // append elements 
+  tasksToDoEl.appendChild(listItemEl);
+  console.dir(listItemEl)
 };
 
 formEl.addEventListener("submit", createTaskHandler);
